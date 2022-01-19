909b8e17f7a360b58c591a534250004d7edb25c0
Ryan Toronto <ryanto@gmail.com>
2022-01-19 14:41:30 -0500

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,48 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { ImageDetail } from "../components/image-detail";
+import { Modal } from "../components/modal";
 
 export default function Home() {
+  let router = useRouter();
   return (
     <div>
+      {router.query.image && (
+        <Modal
+          onClose={() => {
+            router.push("/");
+          }}
+        >
+          <ImageDetail image={router.query.image} />
+        </Modal>
+      )}
       <div className="grid w-screen h-screen grid-cols-3 grid-rows-2">
-        <Link href="/1">
+        <Link href="/?image=1" as="/1">
           <a>
             <img src="/1.jpeg" className="object-cover w-full h-full" />
           </a>
         </Link>
-        <Link href="/2">
+        <Link href="/?image=2" as="/2">
           <a>
             <img src="/2.jpeg" className="object-cover w-full h-full" />
           </a>
         </Link>
-        <Link href="/3">
+        <Link href="/?image=3" as="/3">
           <a>
             <img src="/3.jpeg" className="object-cover w-full h-full" />
           </a>
         </Link>
-        <Link href="/4">
+        <Link href="/?image=4" as="/4">
           <a>
             <img src="/4.jpeg" className="object-cover w-full h-full" />
           </a>
         </Link>
-        <Link href="/5">
+        <Link href="/?image=5" as="/5">
           <a>
             <img src="/5.jpeg" className="object-cover w-full h-full" />
           </a>
         </Link>
-        <Link href="/6">
+        <Link href="/?image=6" as="/6">
           <a>
             <img src="/6.jpeg" className="object-cover w-full h-full" />
           </a>
